220b0f02a43ad70419138de31f762bde5cc01023
unknown <halder25572@gmail.com.com>
2023-01-09 21:45:33 +0600

    successfull update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import CartProduct from '../CartProduct/CartProduct';
 import './Cart.css';
 
 const Cart = (props) => {
@@ -5,17 +6,20 @@ const Cart = (props) => {
     console.log(carts);
 
 
-    let total = '';
+    // let total = '';
 
-    for(const names of carts){
-       total = total + names.name;
-    }
+    // for(const names of carts){
+    //    total = total + names.name;
+    // }
 
     return (
         <div className='cart'>
             <h2>Selected Fruit</h2>
             <div className='output'>
-                <h4>{total}</h4>
+                {/* <h4>{total}</h4> */}
+                {
+                    carts.map(total => <CartProduct total={total}></CartProduct>)
+                }
             </div>
             <button className='cart-btn1'>Choose 1 for Me</button>
             <br />
--- src/components/CartProduct/CartProduct.js
+++ src/components/CartProduct/CartProduct.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './CartProduct.css';
 
 const CartProduct = (props) => {
+
+    const {picture, name} = props.total;
+
     return (
-        <div>
-            <h4>{props.total}</h4>
+        <div className='cart-product'>
+            <img src={picture} alt="" />
+            <h4>{name}</h4>
         </div>
     );
 };
